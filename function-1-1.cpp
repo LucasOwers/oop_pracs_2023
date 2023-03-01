6e3d7f405cab46e7a81c3a53de1cd9bcab24d350
Lucas Owers <elpookyvg@gmail.com>
2023-03-01 05:58:47 +0000

    question-1-1

---
+++ function-1-1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+//function declaration
+int sum(int array[], int size){
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum = sum+array[i];
+    }
+    return sum;
+};
