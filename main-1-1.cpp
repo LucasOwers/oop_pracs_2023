7d70bef5be6a3555e8ef5ca31d6fff071f3b6c8d
Lucas Owers <elpookyvg@gmail.com>
2023-03-02 02:11:11 +0000

    question-1-1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -2,7 +2,7 @@
 
 // The `extern` keyword tells the compiler that the count function exists,
 // but will be implemented somewhere else
-extern int array_sum(int array[], int n)
+extern int array_sum(int array[], int n);
 
 int main() {
     int array[5] = {4,5,6,9,8};
