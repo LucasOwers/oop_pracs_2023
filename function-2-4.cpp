c92b5d7fd88cfaa5784a89aa60d909c5ad1a3b26
Lucas Owers <elpookyvg@gmail.com>
2023-03-03 18:17:23 +0000

    question2-4 again

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -4,7 +4,7 @@ bool is_ascending(int array[], int n){
     if (n <= 0){
         return false;
     }
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i <= n; i++){
         if (array[i]>array[i+1]){
             return false;
         }
