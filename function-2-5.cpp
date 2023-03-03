1750d7506412deed9de36c564c1466d389120d9e
Lucas Owers <elpookyvg@gmail.com>
2023-03-03 18:34:14 +0000

    test

--- function-2-5.cpp
+++ function-2-5.cpp
@@ -5,7 +5,7 @@ bool is_descending(int array[], int n){
         return false;
     }
     for (int i = 0; i < n; i++){
-        if (array[i]>array[i-1]){
+        if (array[i]<array[i-1]){
             return false;
         }
 
