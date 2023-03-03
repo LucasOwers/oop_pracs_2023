3a7d7638c5be9407287158a7c013bae26e8a6cba
Lucas Owers <elpookyvg@gmail.com>
2023-03-03 18:40:29 +0000

    question2-4

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -3,7 +3,7 @@
 extern bool is_ascending(int array[], int n);
 
 int main(){
-    int array[5] = {1,5,6,7,9};
+    int array[5] = {1,0,0,0,0};
     std::cout << is_ascending(array, 5) << std::endl;
     return 0;
 }
