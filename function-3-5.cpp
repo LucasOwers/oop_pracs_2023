ac7b246203e7afd2d1ae5902862cab6b6e54ddfd
Lucas Owers <elpookyvg@gmail.com>
2023-03-04 20:44:37 +0000

    question3-5

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -5,7 +5,7 @@ double sum_even(double array[], int n){
         return 0;
     }
     double sum_even = 0;
-    for (int i = 1; i < n; i += 2 ){
+    for (int i = 0; i < n; i += 2 ){
         sum_even = sum_even+array[i];
     }
     return sum_even;
