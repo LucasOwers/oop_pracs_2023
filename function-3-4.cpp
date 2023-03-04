92f7391edb5a53ea49877f5288587764e75d01d0
Lucas Owers <elpookyvg@gmail.com>
2023-03-04 19:11:41 +0000

    question3-4

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -4,16 +4,16 @@ void print_pass_fail(char grade){
 
     switch (grade){
         default:
-            std::cout << "Nothing.\n";
+            std::cout << "Nothing\n";
             break;
         case 'E':
         case 'D':
-             std::cout << "Fail.\n";
+             std::cout << "Fail\n";
              break;
         case 'C':
         case 'B':
         case 'A':
-             std::cout << "Pass.\n";
+             std::cout << "Pass\n";
              break;
     }
 }
