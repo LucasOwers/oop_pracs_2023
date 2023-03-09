eaa0bc34160304f987aae8eb888c0f6ee978885d
Lucas Owers <elpookyvg@gmail.com>
2023-03-09 16:47:54 +0000

    question1-4

---
+++ main-1-4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+void print_scaled(int array[3][3],int scale);
+
+int main(){
+    int array[3][3] = {{ 1, 2, 3},
+                       { 5, 6, 7},
+                       { 9, 1, 2}};
+    int scale = 3;
+    print_scaled(array, scale);
+    return 0;
+
+}
