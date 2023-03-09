067a6b6d36241c93867fcc2ed73f861519f3be7a
Lucas Owers <elpookyvg@gmail.com>
2023-03-09 16:56:48 +0000

    again

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,7 @@ void print_scaled(int array[3][3],int scale){
     for (int i = 0; i < 3; i++){
          for (int j = 0; j< 3; j++){
              array[i][j] = (array[i][j])*scale;
-             std::cout << " " << array[i][j];
+             std::cout << array[i][j] << " ";
          }
     std::cout << std::endl;
     }
