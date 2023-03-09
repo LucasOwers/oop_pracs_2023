b3f17068c8345dc4716b057599526c951950978c
Lucas Owers <elpookyvg@gmail.com>
2023-03-09 16:53:14 +0000

    question1-4

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -3,9 +3,9 @@
 void print_scaled(int array[3][3],int scale);
 
 int main(){
-    int array[3][3] = {{ 1, 2, 3},
-                       { 5, 6, 7},
-                       { 9, 1, 2}};
+    int array[3][3] = {{1, 2, 3},
+                       {5, 6, 7},
+                       {9, 1, 2}};
     int scale = 3;
     print_scaled(array, scale);
     return 0;
