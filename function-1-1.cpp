e8a35611edfff7ec64bd0d88a2bd4bc909945199
Lucas Owers <elpookyvg@gmail.com>
2023-03-22 15:47:18 +0000

    q1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 void print_matrix(int array[10][10]){
+    j = 0;
     for (int i = 0; i < 10; i++){
+        cout << array[1][j];
         for (int j = 0; j < 10; j++){
             cout << "" << array[i][j];
         }
