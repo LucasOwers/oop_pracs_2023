de702b75f196f8212bc66ae5f61666f515a4a154
Lucas Owers <elpookyvg@gmail.com>
2023-07-24 03:00:40 +0000

    question3-1update

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -6,15 +6,17 @@ bool is_fanarray(int array[], int n){
     }
     int center = n/2;
 
-    for (int i = 1; i < center; i++){
-        if (array[i]<array[i-1]){
-            return false;
-        }
+    if (n > 1){
+        for (int i = 1; i < center; i++){
+            if (array[i]<array[i-1]){
+                return false;
+            }
 
-    }
-    for (int i = center; i < n; i++){
-        if (array[i]>array[i-1]){
-            return false;
+        }
+        for (int i = center; i < n; i++){
+            if (array[i]>array[i-1]){
+                return false;
+            }
         }
     }
     return true;
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -3,8 +3,8 @@
 extern bool is_fanarray(int array[], int n);
 
 int main(){
-    int array[5] = {0,3,5,3,0};
-    std::cout << is_fanarray(array, 5) << std::endl;
+    int array[1] = {0};
+    std::cout << is_fanarray(array, 1) << std::endl;
     return 0;
 }
 
