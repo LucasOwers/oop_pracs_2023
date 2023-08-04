6b0e3dcbaf2c0b99921bd05c9b581839beba90be
Lucas Owers <elpookyvg@gmail.com>
2023-08-04 07:38:24 +0000

    lessgoooo

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -5,10 +5,10 @@ int array_min(int integers[], int length){
         if (length<=0){
         return -1;
     }
-    int min_number = 0;
+    int min_number=integers[0];
     for (int i = 0; i<length; i++){
-        if (integers[i]<integers[i-1]){
-            integers[i]=min_number;
+        if (integers[i]<min_number){
+            min_number = integers[i];
         }
     }
     return min_number;
@@ -18,10 +18,10 @@ int array_max(int integers[], int length){
         if (length<=0){
         return -1;
     }
-        int max_number = integers[0];
+    int max_number=integers[0];
     for (int i = 0; i<length; i++){
-        if (integers[i]>integers[i-1]){
-            integers[i]=max_number;
+        if (integers[i]>max_number){
+            max_number = integers[i];
         }
     }
     return max_number;
@@ -32,6 +32,6 @@ int sum_min_max(int integers[], int length){
         return -1;
     }
     int sum = 0;
-    sum = array_min(integers, length) + array_max(integers, length);
+    sum = array_min(integers, length)+array_max(integers, length);
     return sum;
 }
--- main-2-4.cpp
+++ main-2-4.cpp
@@ -7,7 +7,7 @@ extern int array_max(int integers[], int length);
 
 int main(){
     int length = 6;
-    int integers[6] = {6,0,1,1,0,6};
+    int integers[6] = {1,2,3,4,5,7};
     int sum = sum_min_max(integers, 6);
     std::cout << " " << sum << std::endl;
     return 0;
