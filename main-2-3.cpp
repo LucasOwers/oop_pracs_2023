7e3da65bfb689893cb7b1be9e6d062fb5eef677c
Lucas Owers <elpookyvg@gmail.com>
2023-08-11 07:30:23 +0000

    question3

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -6,7 +6,7 @@ extern int palindrome_sum(int integers[], int length);
 
 int main(){
     int length = 4;
-    int integers[4] = {1,2,2,1};
+    int integers[4] = {1,2,2,1,};
     int sum = palindrome_sum(integers,length);
     std::cout << sum << std::endl;
     return 0;
